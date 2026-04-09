0bd952adab0fcf54bae4fb4e353b507f264da242
Stoian Cristian <stoiancristian2002@gmail.com>
2026-04-09 13:40:57 +0300

    task 1-3 completed

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ df_daily = pl.read_csv("data/daily_*.csv")
 df_monthly = pl.read_csv("data/monthly_*.csv")
 # print(df_monthly)
 
+df_accounts = df_accounts.with_columns(pl.col("account_id").cast(pl.Int64))
+df_monthly = df_monthly.with_columns(pl.col("account").cast(pl.Int64))
+df_daily = df_daily.with_columns([
+    pl.col("account").cast(pl.Int64),
+    pl.col("changed_datetime").str.to_datetime(strict=False)
+])
+
 load_dotenv()
 db_password = os.getenv("DB_PASSWORD")
 port = os.getenv("PORT")
@@ -33,3 +40,15 @@ df_monthly.write_database(
     connection=engine,
     if_table_exists="replace"
 )
+
+query = """SELECT a.account_id, a.name, a.address, ds.queue, ds.status, ds.changed_datetime 
+    FROM "Accounts" as a 
+    LEFT JOIN "Daily Status" as ds ON a.account_id = ds.account 
+    WHERE changed_datetime >= TO_DATE('2025-01-01','YYYY-MM-DD');
+"""
+
+df_rez = pl.read_database(query=query, connection=engine)
+df_rez = df_rez.with_columns(pl.col("changed_datetime").dt.strftime("%Y-%m-%d %H:%M:%S"))
+# print(df_rez)
+
+df_rez.write_csv("results/rezultat_1-3.csv")
